15fe09c77a91ddb5b46d651756b602bc06b8f2c0
Joost van Veen <joost@accentinteractive.nl>
2014-07-28 18:15:25 +0200

    Lesson 14 - Laravel 4 database seeding

---
+++ app/database/seeds/PostsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class PostsTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+
+		foreach(range(1, 10) as $index)
+		{
+			Post::create([
+                'title' => $faker->sentence(),
+                'body' => $faker->realText(500),
+                'user_id' => rand(1,2)
+			]);
+		}
+	}
+
+}
